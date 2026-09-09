42d0e661dbc42b37025782d3bd3c9169875b0ee3
MESBAHI-Nour <s3487637@email.com>
2026-09-09 12:30:29 +0100

    Algo repitition

---
+++ Algorithm/repetitions.js
@@ -0,0 +1,42 @@
+let nombres = [4, 7, 2, 7, 9, 4,5];
+console.log(`tableau de depart: ${nombres}`)
+let tab=[]
+for (let i=0;i<nombres.length;i++){
+    for (let j=0;j<nombres.length;j++){
+        if(nombres[i]===nombres[j] && j!==i){
+            if(!tab.includes(nombres[i])){
+                tab.push(nombres[i])
+            }
+        }
+    }    
+}
+console.log(`Nouveau tableau: ${tab}`)
+
+///////////////////////////////////////////
+
+let nb = [4, 7, 2, 7, 9, 4, 5];
+console.log(`Tableau de départ : ${nb}`);
+let tab1=[];
+let len=0;
+for (let i=0; i<nb.length; i++) {
+    let compteur=0;
+    for (let j=0; j<nb.length; j++) {
+        if (nb[i]===nb[j]) {
+            compteur++;
+        }
+    }
+    if (compteur >= 2) {
+        let existe=false;
+        for (let k=0; k<len; k++) {
+            if (tab1[k] === nb[i]) {
+                existe = true;
+                break;
+            }
+        }
+        if (existe===false) {
+            tab1[len]=nb[i];
+            len++;
+        }
+    }
+}
+console.log(`Nouveau tableau : ${tab1}`);
