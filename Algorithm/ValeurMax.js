b5eee4e7ae4d072f2a29e2cc400deb1b2098633b
MESBAHI-Nour <s3487637@email.com>
2026-09-14 15:34:49 +0100

    update the structure

--- Algorithm/ValeurMax.js
+++ Algorithm/ValeurMax.js
@@ -1,8 +1,8 @@
-let A=20
-let B=10
-let C=0
-console.log(`Avant A=${A} B=${B}`)
-C=A
-A=B
-B=C
-console.log(`Apres A=${A} B=${B}`)
+const tab=[2,5,3,10]
+let max=tab[0]
+for(let i=1;i<=tab.length-1;i++){
+    if(tab[i]>max){
+        max=tab[i]
+    }
+}
+console.log(max)
