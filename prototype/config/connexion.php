36da1940bc8c7d38797808ecdc8bcc6aa9549856
MESBAHI-Nour <s3487637@email.com>
2026-09-16 16:53:56 +0100

    update admin pages

--- prototype/config/connexion.php
+++ prototype/config/connexion.php
@@ -1,7 +1,6 @@
 <?php
 try{
     $connexion=new PDO("mysql:host=localhost;dbname=plateforme_streaming;port=3306","root","root");
-    echo "connexion reussi";
 }
 catch(Exception $e){
     echo $e;
--- prototype/index.php
+++ prototype/index.php
@@ -1,4 +1,10 @@
-
+<?php
+    require_once('config/connexion.php');
+    $req=$connexion->prepare("SELECT * FROM film f INNER JOIN realisateur r ON f.id_realisateur=r.id_realisateur
+    INNER JOIN genre g ON f.id_genre=g.id_genre");
+    $req->execute();
+    $films=$req->fetchAll(); 
+?>
 
 
 <!DOCTYPE html>
@@ -11,4 +17,31 @@
 <body>
     
 </body>
+    <main>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID Film</th>
+                    <th>Poster</th>
+                    <th>Titre</th>
+                    <th>Genre</th>
+                    <th>Date de sortie</th>
+                    <th>Duree</th>
+                    <th>classification d'age</th>
+                    <th>Realisateur</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    foreach($films as $film){
+                ?>
+                    <tr>
+                        <td><?= $film["id_film"] ?></td>
+                    </tr>
+                <?php
+                    }
+                ?>
+            </tbody>
+        </table>
+    </main>
 </html>
