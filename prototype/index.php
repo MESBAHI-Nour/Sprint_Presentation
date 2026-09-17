25e5f16fe5ffd79b4fe63bc503e05dd8d5cd0b68
MESBAHI-Nour <s3487637@email.com>
2026-09-17 09:55:03 +0100

    update pages index.php

--- prototype/index.php
+++ prototype/index.php
@@ -13,11 +13,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style/index.css">
 </head>
 <body>
     
 </body>
     <main>
+        <h1>Gestion Des Film</h1>
+        <a href="admin/admin-film-form.php">Ajouter Film</a>
         <table>
             <thead>
                 <tr>
@@ -37,6 +40,13 @@
                 ?>
                     <tr>
                         <td><?= $film["id_film"] ?></td>
+                        <td><img src="<?= $film["poster_film_url"] ?>"></td>
+                        <td><?= $film["titre_film"] ?></td>
+                        <td><?= $film["nom_genre"] ?></td>
+                        <td><?= $film["date_sortie"] ?></td>
+                        <td><?= $film["duree"] ?></td>
+                        <td><?= $film["classification_age"] ?></td>
+                        <td><?= $film["nom_realisateur"] ?></td>
                     </tr>
                 <?php
                     }
